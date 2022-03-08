92a1008de673b456ece44fbbb58b771e59f888a0
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-08 20:22:19 +0000

    tested project component

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
+  collectCoverageFrom: ["./src/**/*.tsx", "!./src/**/main.tsx"],
+};
---
+++ src/components/Project.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import Project from "./Project";
+
+describe("Given a project component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should show 2 images and 4 buttons with accessible names: like, coments, share, bookmark", () => {
+      const expectedButtons = ["like", "comments", "share", "bookmark"];
+      const expectedNumberOfImages = 2;
+
+      render(<Project />);
+      const images = screen.getAllByRole("img");
+      const buttons = expectedButtons.map((buttonName) =>
+        screen.getByRole("button", { name: buttonName })
+      );
+
+      expect(images).toHaveLength(expectedNumberOfImages);
+      buttons.forEach((button) => expect(button).toBeInTheDocument());
+    });
+  });
+});
