322878b5c1e5d64d26ec14682563f812a211f93c
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-08 19:11:36 +0000

    created component Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Project from "./components/Project";
+
 function App() {
-  return <h1>helloworld</h1>;
+  return (
+    <>
+      <h1>helloworld</h1>
+      <Project />
+    </>
+  );
 }
 
 export default App;
