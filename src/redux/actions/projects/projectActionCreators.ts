06fb227eda1cc7a6cf63063dc7d3b13b379da773
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-09 18:26:00 +0000

    created reducer for projects

--- src/redux/actions/projects/projectActionCreators.ts
+++ src/redux/actions/projects/projectActionCreators.ts
@@ -1,7 +1,11 @@
 import { Action } from "../../../types/actionTypes";
+import { ProjectShape } from "../../../types/projectTypes";
 import projectActionTypes from "./projectActionTypes";
 
 // eslint-disable-next-line import/prefer-default-export
-export const loadProjectsActionCreator = (): Action => ({
+export const loadProjectsActionCreator = (
+  projects: ProjectShape[]
+): Action => ({
   type: projectActionTypes.loadProjects,
+  projects,
 });
