5ac74160bfc175f0bed59181f3c22685ecabda66
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-16 20:19:22 +0100

    fixed navigate on login and register

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,13 @@
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import UserForm, { UserFormData } from "../components/UserForm/UserForm";
 import { loginUserThunk } from "../redux/thunks/user/userThunks";
 
 const Login = (): JSX.Element => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const onSubmit = (user: UserFormData) => {
-    dispatch(loginUserThunk(user));
+    dispatch(loginUserThunk(user, navigate));
   };
 
   return <UserForm isLogin onSubmit={onSubmit} />;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,13 @@
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import UserForm, { UserFormData } from "../components/UserForm/UserForm";
 import { registerUserThunk } from "../redux/thunks/user/userThunks";
 
 const Register = (): JSX.Element => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const onSubmit = (user: UserFormData) => {
-    dispatch(registerUserThunk(user));
+    dispatch(registerUserThunk(user, navigate));
   };
 
   return <UserForm onSubmit={onSubmit} />;
